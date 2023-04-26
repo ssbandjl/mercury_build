d9aa7c7643dcebf809977eeeab1e33fa2ac9ea95
xb <ssbandjl@163.com>
2023-04-26 07:03:27 +0000

    xb_init

---
+++ mercury/05_bulk/CMakeFiles/05_hg_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/05_hg_client.dir/client.o"
+  "05_hg_client.pdb"
+  "05_hg_client"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/05_hg_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ mercury/05_bulk/CMakeFiles/05_hg_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/05_hg_server.dir/server.o"
+  "05_hg_server.pdb"
+  "05_hg_server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/05_hg_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
